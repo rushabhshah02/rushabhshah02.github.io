6213b5c77a7b9d0762209338a25b7724ce701e5e
Rushabh Shah <rnshah@ualberta.ca>
2024-06-15 10:23:28 -0600

    Made the Name into a typing animation

---
+++ script.js
@@ -0,0 +1,8 @@
+// TYPING ANIMATION
+var typed = new Typed(".animation-home", {
+    strings: ["Rushabh Shah", "a Developer", "a CS major"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
